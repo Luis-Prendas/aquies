ffc074869b89b2f0149ac928ebabb1e632312b53
Luis Prendas <luisprendas.dev@gmail.com>
2025-09-02 11:32:41 -0600

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,44 @@ import img3 from './assets/img-3.webp'
 import img4 from './assets/img-4.webp'
 import aquiesLogo from './assets/aquies.jpg'
 import Article from './components/Article'
+import { useState } from 'react'
+import Modal from './components/Modal'
 
 function App() {
+  const [open, setOpen] = useState(false)
+  const [modalContent, setModalContent] = useState<{ title: string, description: string, image: string } | null>(null)
+
+  const handleOpenModal = ({ title, description, image }: { title: string, description: string, image: string }) => {
+    setModalContent({ title, description, image })
+    setOpen(true)
+  }
+
   return (
     <>
+      {open && modalContent && (
+        <Modal
+          image={modalContent.image}
+          open={open}
+          setOpen={setOpen}
+          title={modalContent.title}
+          description={modalContent.description}
+        />
+      )}
       <main className='flex justify-center items-center w-screen min-h-screen relative bg-black'>
-        <img src={aquiesLogo} alt="Aquies Logo" className='absolute top-4 left-4 w-32 h-32 object-contain rounded-lg z-20 opacity-40 cursor-pointer hover:opacity-100 hover:scale-105 transition-all duration-300' />
+        <img src={aquiesLogo} alt="Aquies Logo" className='absolute top-4 left-4 w-32 object-contain rounded-lg z-20 opacity-40 cursor-pointer hover:opacity-100 hover:scale-105 transition-all duration-300' />
         <video
           src={videoBackGround}
           autoPlay
           loop
           muted
           playsInline
-          className='fixed top-0 left-0 w-screen h-screen object-cover opacity-20'
+          className='fixed top-0 left-0 w-screen h-screen object-cover opacity-70'
         />
-        <section className='flex justify-center items-center z-10 flex-wrap gap-12 w-full h-full p-4 lg:px-[300px]'>
-          <Article img={img1} title="Image 1" description="Description for image 1" />
-          <Article img={img2} title="Image 2" description="Description for image 2" />
-          <Article img={img3} title="Image 3" description="Description for image 3" />
-          <Article img={img4} title="Image 4" description="Description for image 4" />
+        <section className='flex justify-center items-center z-10 flex-wrap gap-12 w-full h-full pb-40'>
+          <Article openModal={handleOpenModal} img={img1} title="Image 1" description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged." />
+          <Article openModal={handleOpenModal} img={img2} title="Image 2" description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged." />
+          <Article openModal={handleOpenModal} img={img3} title="Image 3" description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged." />
+          <Article openModal={handleOpenModal} img={img4} title="Image 4" description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged." />
         </section>
       </main>
     </>
--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -1,10 +1,10 @@
-export default function Article({ img, title, description }: { img: string, title: string, description: string }) {
+export default function Article({ img, title, description, openModal }: { img: string, title: string, description: string, openModal: Function }) {
   return (
     <a href="#" className='bg-black/20 border border-white/10 backdrop-blur-lg rounded-lg overflow-hidden hover:scale-105 transition-transform duration-300'>
-      <img src={img} alt={img} className="w-[400px] h-[250px] object-cover lg:w-[500px] lg:h-[300px]" />
+      <img src={img} alt={img} className="w-[300px] h-[150px] object-cover" />
       <div className='p-4'>
         <h2 className='font-bold text-xl text-white'>{title}</h2>
-        <p className='text-sm text-white/80'>{description}</p>
+        <p className='text-sm text-white/80 hover:underline truncate w-[200px]' onClick={() => openModal({ title, description, image: img })}>{description}</p>
       </div>
     </a>
   )
